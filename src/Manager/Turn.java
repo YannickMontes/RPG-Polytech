9317d616578419207dce9518f5e9d433bf1840b1
Corentin <corentin.marechal@gmail.com>
2016-01-07 09:55:59 +0100

    Modification exécution tour

--- src/Manager/Turn.java
+++ src/Manager/Turn.java
@@ -32,68 +32,72 @@ public class Turn {
 
     public boolean executeTurn() {
         for (Character character : team.getCharacters()) {
-            character.restoreAttributes();
-            String text = "C'est votre tour: " + character.getName() + "\n"
-            + "--------------------\n"
-            + "Quel action voulez-vous réaliser ?\n"
-            + "1 -> Attaquer un personnage\n"
-            + "2 -> Utiliser une parade\n"
-            + "3 -> Utiliser un soin";
-            int actionNumber = controller.askNumberBetween(text, 1, 3);
-            switch (actionNumber) {
-                case 1:
-                    Character opponent = null;
-                    String actionText = "Choississez un adversaire\n";
-                    int num = 0;
-                    for (Character op : opponentsTeam.getCharacters()) {
-                        actionText += Integer.toString(num) + " -> " + op.getName() + "\n";
-                        num++;
-                    }
-                    int opponentNumber = controller.askNumberBetween(actionText,0, num);
-
-                    opponent = opponentsTeam.getCharacters().get(opponentNumber);
-                    if (character instanceof Warrior) {
-                        System.out.println(((Warrior) character).strikeABlow(opponent));
-                    } else if (character instanceof Athlete) {
-                        System.out.println(((Athlete) character).strikeABlow(opponent));
-                    } else if (character instanceof Wizard) {
-                        System.out.println(((Wizard) character).strikeABlow(opponent));
-                    }
-                    break;
-                case 2:
-                    if (character instanceof Warrior) {
-                        ((Warrior) character).block();
-                        ((Warrior) character).dodge();
-                    } else if (character instanceof Athlete) {
-                        ((Athlete) character).block();
-                        ((Athlete) character).dodge();
-                    } else if (character instanceof Wizard) {
-                        ((Wizard) character).block();
-                        ((Wizard) character).dodge();
-                    }
-                    break;
-                case 3:
-                    UseableItem useableItem = null;
-                    if (character.numberUseableItem() > 0) {
-                        String careText = "Choississez un soin";
-                        int numCare = 0;
-                        for (Item item : character.getInventory()) {
-                            if (item instanceof UseableItem) {
-                                System.out.println(Integer.toString(numCare) + " -> " + item.getName() + " de bonus " + ((UseableItem) item).getBonusValue());
-                            }
-                            numCare++;
+            if (character.isAlive()) {
+                character.restoreAttributes();
+                String text = "C'est votre tour: " + character.getName() + "\n"
+                        + "--------------------\n"
+                        + "Quel action voulez-vous réaliser ?\n"
+                        + "1 -> Attaquer un personnage\n"
+                        + "2 -> Utiliser une parade\n"
+                        + "3 -> Utiliser un soin";
+                int actionNumber = controller.askNumberBetween(text, 1, 3);
+                switch (actionNumber) {
+                    case 1:
+                        Character opponent = null;
+                        String actionText = "Choississez un adversaire\n";
+                        int num = 0;
+                        for (Character op : opponentsTeam.getCharacters()) {
+                            actionText += Integer.toString(num) + " -> " + op.getName() + "\n";
+                            num++;
                         }
-                        int useableNumber = controller.askNumberBetween(careText,0, numCare);
-                        useableItem = (UseableItem) character.getInventory().get(useableNumber);
-                    }
-                    if (character instanceof Warrior) {
-                        System.out.println(((Warrior) character).heal(useableItem));
-                    } else if (character instanceof Athlete) {
-                        System.out.println(((Athlete) character).heal(useableItem));
-                    } else if (character instanceof Wizard) {
-                        System.out.println(((Wizard) character).heal(useableItem));
-                    }
-                    break;
+                        int opponentNumber = controller.askNumberBetween(actionText, 0, num);
+
+                        opponent = opponentsTeam.getCharacters().get(opponentNumber);
+                        if (character instanceof Warrior) {
+                            System.out.println(((Warrior) character).strikeABlow(opponent));
+                        } else if (character instanceof Athlete) {
+                            System.out.println(((Athlete) character).strikeABlow(opponent));
+                        } else if (character instanceof Wizard) {
+                            System.out.println(((Wizard) character).strikeABlow(opponent));
+                        }
+                        break;
+                    case 2:
+                        if (character instanceof Warrior) {
+                            ((Warrior) character).block();
+                            ((Warrior) character).dodge();
+                        } else if (character instanceof Athlete) {
+                            ((Athlete) character).block();
+                            ((Athlete) character).dodge();
+                        } else if (character instanceof Wizard) {
+                            ((Wizard) character).block();
+                            ((Wizard) character).dodge();
+                        }
+                        break;
+                    case 3:
+                        UseableItem useableItem = null;
+                        if (character.numberUseableItem() > 0) {
+                            String careText = "Choississez un soin";
+                            int numCare = 0;
+                            for (Item item : character.getInventory()) {
+                                if (item instanceof UseableItem) {
+                                    System.out.println(Integer.toString(numCare) + " -> " + item.getName() + " de bonus " + ((UseableItem) item).getBonusValue());
+                                }
+                                numCare++;
+                            }
+                            int useableNumber = controller.askNumberBetween(careText, 0, numCare);
+                            useableItem = (UseableItem) character.getInventory().get(useableNumber);
+                        }
+                        if (character instanceof Warrior) {
+                            System.out.println(((Warrior) character).heal(useableItem));
+                        } else if (character instanceof Athlete) {
+                            System.out.println(((Athlete) character).heal(useableItem));
+                        } else if (character instanceof Wizard) {
+                            System.out.println(((Wizard) character).heal(useableItem));
+                        }
+                        break;
+                }
+            }else{
+                System.out.println("Le joueur "+character.getName()+" est mort !");
             }
         }
         return true;

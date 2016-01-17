9dce5d8c20997bc3b4e6e5e542339b8066df76de
Corentin <corentin.marechal@gmail.com>
2016-01-17 16:52:14 +0100

    Modification du texte (Ex: votre attaque a échoué en L'attaque a échoué) pour l'affichage

--- src/Entities/Character.java
+++ src/Entities/Character.java
@@ -504,7 +504,7 @@ public abstract class Character
         {
             return "Vous avez infligé " + damages + " de dégâts à " + opponent.getName() + " (Santé: " + opponent.getAttributes().get(Attribute.HEALTH) + ")";
         }
-        return "Votre attaque sur " + opponent.getName() + " a échoué";
+        return "L'attaque sur " + opponent.getName() + " a échoué";
     }
 
     public String careResult(boolean success, int care)
@@ -513,7 +513,7 @@ public abstract class Character
         {
             return "Vous avez augmenté de " + care + " votre vie " + " (Santé: " + this.getAttributes().get(Attribute.HEALTH) + ")";
         }
-        return "Votre soin a échoué";
+        return "Le soin a échoué";
     }
 
     public String blockResult(boolean success, int blockValue)
@@ -522,7 +522,7 @@ public abstract class Character
         {
             return "Vous avez augmenté de " + blockValue + " votre défense " + " (Défense: " + this.getAttributes().get(Attribute.DEFENSE) + ")";
         }
-        return "Votre blocage a échoué";
+        return "Le blocage a échoué";
     }
 
     public String dodgeResult(boolean success, int blockValue)
@@ -531,7 +531,7 @@ public abstract class Character
         {
             return "Vous avez augmenté de " + blockValue + " votre défense " + " (Défense: " + this.getAttributes().get(Attribute.DEFENSE) + ")";
         }
-        return "Votre esquive a échoué";
+        return "L'esquive a échoué";
     }
 
     public void putRandomPoint(int lvl)

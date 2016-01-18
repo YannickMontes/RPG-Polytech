8153033fddd35fba02115c2f202bc89f53d6a615
MONTES Yannick <montes.yannick@gmail.com>
2016-01-19 00:07:25 +0100

    Ajout de deux nouvelles sous-classes de UseableItem, Potion et sortilège.

---
+++ src/Items/Potion.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Items;
+
+import Controller.ConsoleDesign;
+import Entities.Attribute;
+import java.util.List;
+
+/**
+ *
+ * @author yannick
+ */
+public class Potion extends UseableItem
+{
+    public static List<UseableItem> listPotionItem;
+    private Attribute attributeUped;
+    
+    public Potion(String name, int weight, int bonusValue, int r, Attribute att)
+    {
+        super(name, weight, bonusValue, r);
+        this.attributeUped = att;
+    }
+    
+    public Attribute getAttributeUped()
+    {
+        return this.attributeUped;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return ConsoleDesign.text("Type: Sortilège\n"
+                +"Attribut augmenté: "+this.attributeUped+"\n"
+                +"Bonus: "+this.getValue(), ConsoleDesign.redText);
+    }
+}
---
+++ src/Items/Sortilege.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Items;
+
+import Controller.ConsoleDesign;
+import java.util.List;
+
+/**
+ *
+ * @author yannick
+ */
+public class Sortilege extends UseableItem
+{
+    public static List<UseableItem> listSortilegeItem;
+    private int manaCost;
+    
+    public Sortilege(String name, int weight, int bonusValue, int rarity, int manaCost)
+    {
+        super(name, weight, bonusValue, rarity);
+        this.manaCost = manaCost;
+    }
+    
+    public int getManaCost()
+    {
+        return this.manaCost;
+    }
+
+    @Override
+    public String toString()
+    {
+        return ConsoleDesign.text("Type: Sortilège\n"
+                +"Dégats: "+this.getValue()+"\n"
+                +"Cout en mana: "+this.manaCost+"\n", ConsoleDesign.redText);
+    }
+}
